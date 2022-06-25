b33737813071cfd4158611be6a521e18f4de1e67
Gordon <>
2022-06-25 15:47:43 +0100

    Fixed lbrandom_choice

--- Maths/LBRandom.lua
+++ Maths/LBRandom.lua
@@ -3,6 +3,8 @@ require("LifeBoatAPI.Utils.LBCopy")
 ---@section LBRandom 1 LBRANDOMCLASS
 ---@class LBRandom
 LifeBoatAPI.LBRandom = {
+
+    ---@return LBRandom
     new = function(cls, seed)
         return LifeBoatAPI.lb_copy(cls, {
             seed = seed or 1
@@ -11,6 +13,7 @@ LifeBoatAPI.LBRandom = {
 
     ---@section lbrandom_next
     ---@param self LBRandom
+    ---@return number 0.001 -> 0.999
     lbrandom_next = function(self)
         self.seed = ((self.seed * 1103515245) + 12345) % 65564
         return (self.seed+1)/65565
@@ -18,10 +21,13 @@ LifeBoatAPI.LBRandom = {
     ---@endsection
 
     ---@section lbrandom_choice
+    ---@generic T type of element
     ---@param self LBRandom
+    ---@param tbl T[]
+    ---@return T
     lbrandom_choice = function(self, tbl)
         local rnd = self:lbrandom_next()
-        return math.ceil(rnd * #tbl)
+        return tbl[math.ceil(rnd * #tbl)]
     end;
     ---@endsection
 }
